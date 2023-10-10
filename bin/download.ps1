78ede17eee529a6c87ad8e96224d8e549a10def6
abgox <abgohxf@outlook.com>
2023-10-10 12:45:42 +0800

    feat(bin): update download.ps1

--- bin/download.ps1
+++ bin/download.ps1
@@ -1,31 +1,48 @@
-param(
-    [string]$app_name
-    )
-$path = (scoop info $app_name -v).Manifest
-$info = Get-Content $path | ConvertFrom-Json
+param($app)
+$scoop_dir = (Split-Path (Split-Path (Split-Path  (scoop prefix scoop) -Parent) -Parent) -Parent)
+$shims_dir = $scoop_dir + '\shims'
+$isExist_aria2 = Get-ChildItem $shims_dir | Where-Object { $_.Name -eq "aria2c.exe" }
+$is_enable_aria = scoop config aria2-enabled
 
-if ($info.url) {
-    $result = $info.url
-} elseif ($info.architecture) {
-    $a = $info.architecture
-    if ($a.'64bit') {
-        $result = $a.'64bit'.url
-    } elseif ($a.'32bit') {
-        $result = $a.'32bit'.url
-    } else {
-        throw "There is no suitable download path"
+if (!$isExist_aria2) {
+    Write-Host "aria2 没有安装" -f Yellow
+    Write-Host "正在安装 aria2" -f Cyan
+    try {
+        scoop install aria2 -u
     }
-} else {
-    throw "There is no suitable download path"
+    catch {
+        Write-Host "安装 aria2 失败，请重新尝试" -f Red
+        return
+    }
 }
-$file_name = $result -replace '://', '_'
-$file_name = $file_name -replace '/', '_'
-$file_name = "$app_name#$($info.version)#$file_name"
-Write-Host "下载路径：$result" -f Green
+if (!$is_enable_aria) {
+    Write-Host "正在启用 aria2 下载" -f Cyan
+    scoop config aria2-enabled true
+}
+$cache_dir = $scoop_dir + '\cache'
+$app_txt = "$cache_dir\$app.txt"
+
+$job = Start-Job -ScriptBlock {
+    param($app)
+    scoop install $app -u
+} -ArgumentList $app
+
+while ($true) {
+    if (Test-Path($app_txt)) {
+        Stop-Job -Job $job
+        break
+    }
+}
+$app_content = Get-Content $app_txt | ForEach-Object { $_.Trim() }
+$download_url = $app_content[0]
+$out_dir = $cache_dir
+$out_file = (($app_content | Select-String -Pattern "^out=.*").Matches.Groups[0].Value).Substring(4)
+
+Remove-Item $app_txt -Force
+
+Write-Host "下载路径：$download_url" -f Green
 Write-Host "---------------" -f Cyan
-Write-Host "请点击下载路径进行手动下载`n将下载好的文件路径填写到此处(无引号)：" -f Yellow -NoNewline
-$download_path = Read-Host
-
-$cache_dir = (Split-Path (Split-Path (Split-Path  (scoop prefix scoop) -Parent) -Parent) -Parent) + '\cache'
-
-Move-Item $download_path "$cache_dir\$file_name"
+Write-Host "请点击下载路径进行手动下载`n请输入下载完成的文件路径：" -f Yellow -NoNewline
+$download_path = $(Read-Host) -replace '"', ''
+Move-Item $download_path "$out_dir\$out_file" -Force
+scoop install $app -u
